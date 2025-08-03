4c5e80272b52f65d38814a04ca6fdd61d3d3713b
lukiet <luketsalwa@gmail.com>
2025-08-03 22:21:46 +0300

    function for loading images

--- pacman.js
+++ pacman.js
@@ -21,15 +21,38 @@ let pacmanLeftImage;
 let pacmanRightImage;
 let wallImage;
 
-
-window.onload = function(){
-    board = document.getElementById("board");
-    board.height = boardHeight;
-    board.width = boardWidth;
-    context = board.getContext("2d"); // used to draw on the canvas
-}
+window.onload = function () {
+  board = document.getElementById("board");
+  board.height = boardHeight;
+  board.width = boardWidth;
+  context = board.getContext("2d"); // used to draw on the canvas
+};
 
 function loadImages() {
-     wallImage = new Image();
-     wallImage.src = "images/wall.png";
+  wallImage = new Image();
+  wallImage.src = "images/wall.png";
+
+  blueGhostImage = new Image();
+  blueGhostImage.src = "images/blue-ghost.png";
+
+  pinkGhostImage = new Image();
+  pinkGhostImage.src = "images/pink-ghost.png";
+
+  orangeGhostImage = new Image();
+  orangeGhostImage.src = "images/orange-ghost.png";
+
+  redGhostImage = new Image();
+  redGhostImage.src = "images/red-ghost.png";
+
+  pacmanUpImage = new Image();
+  pacmanUpImage.src = "images/pacman-up.png";
+
+  pacmanDownImage = new Image();
+  pacmanDownImage.src = "images/pacman-down.png";
+
+  pacmanLeftImage = new Image();
+  pacmanLeftImage.src = "images/pacman-left.png";
+  
+  pacmanRightImage = new Image();
+  pacmanRightImage.src = "images/pacman-right.png";
 }
