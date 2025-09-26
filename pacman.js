136be10f68b41d13e97241285119189344c09d90
lukiet <luketsalwa@gmail.com>
2025-09-26 23:53:52 +0300

    added collision function

--- pacman.js
+++ pacman.js
@@ -182,6 +182,13 @@ function movePacman(e){
 }
 }
 
+// colition function
+function collision (a, b){
+  return a.x < b.x + b.width &&
+         a.x + a.width > b.x &&
+         a.y < b.y + b.height &&
+         a.y + a.height > b.y;
+}
 class Block {
   constructor(image, x, y, width, height) {
     this.image = image;
